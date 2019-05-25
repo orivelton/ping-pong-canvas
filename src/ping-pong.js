682afae13f678eed9f2d07067d6e78a03be27593
Örivelton Cesar <oconceicao@recordtv.com.br>
2019-05-25 00:16:27 -0300

    chore: Added a moviment the ball

--- src/ping-pong.js
+++ src/ping-pong.js
@@ -10,6 +10,22 @@ class Rect {
     this.pos = new Vec;
     this.size = new Vec(w,h);
   }
+
+  get left() {
+    return this.pos.x - this.size.x / 2;
+  }
+  
+  get right() {
+    return this.pos.x + this.size.x / 2;
+  }
+
+  get top() {
+    return this.pos.y - this.size.y / 2;
+  }
+
+  get bottom() {
+    return this.pos.y - this.size.y / 2;
+  }
 }
 
 class Ball extends Rect {
@@ -19,29 +35,70 @@ class Ball extends Rect {
   }
 }
 
+class Player extends Rect {
+  constructor () {
+    super(20, 100);
+    this.score = 0;
+  }
+}
+
+
+class Pong {
+  constructor (canvas) {
+    this._canvas = canvas;
+    this._context = canvas.getContext('2d');
+
+    this.ball = new Ball;
+    this.ball.pos.x = 100;
+    this.ball.pos.y = 50;
+
+    this.ball.vel.x = 100;
+    this.ball.vel.y = 100;
+
+    let lastTime;
+
+    const callback = millis => {
+      if(lastTime) {
+        this.update((millis - lastTime) / 1000);
+      }
+
+      lastTime = millis;
+
+      requestAnimationFrame(callback);
+    }
+
+    callback(); 
+  }
+
+  draw() {
+    this._context.fillStyle = '#000000';
+    this._context.fillRect(0, 0, this._canvas.width, this._canvas.height);
+
+    this.drawRect(this.ball);
+  }
+
+  drawRect(rect) {
+    this._context.fillStyle = '#FFFFFF';
+    this._context.fillRect(rect.pos.x, rect.pos.y, rect.size.x, rect.size.y);
+  }
+
+  update(dt) {
+    this.ball.pos.x += this.ball.vel.x * dt;
+    this.ball.pos.y += this.ball.vel.y * dt;
+
+    if(this.ball.left < 0 || this.ball.right > this._canvas.width) {
+      this.ball.vel.x = -this.ball.vel.x;
+    }
+
+    if(this.ball.top < 0 || this.ball.bottom  > this._canvas.height) {
+      this.ball.vel.y = -this.ball.vel.y;
+    }
+
+    this.draw();
+    
+  }
+}
 
 const canvas = document.getElementById('pong');
-const context = canvas.getContext('2d');
-
-const ball = new Ball();
-ball.pos.x = 100;
-ball.pos.y = 50;
-
-ball.vel.x = 100;
-ball.vel.y = 100;
-
-const callback = millis => {
-
-}
-
-const update = dt => {
-  ball.pos.x += ball.vel.x * dt;
-  ball.pos.y += ball.vel.y * dt;
-
-  context.fillStyle = '#000000';
-  context.fillRect(0, 0, canvas.width, canvas.height);
-
-  context.fillStyle = '#FFFFFF';
-  context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
-}
+const pong = new Pong(canvas);
 
